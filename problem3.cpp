dcb505421b15745a8b6ae630b7140d26ed3e11f5
umaraliyev0101 <d.umaraliyev@newuu.uz>
2024-03-06 10:06:19 +0500

    3rd p

--- problem3.cpp
+++ problem3.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
-int main(){
+//class Student{
+//private:
+//    string name;
+//    long int id;
+//    string groupn;
+//public:
+//    Student(string n, long int id, string gn){
+//        this -> name = n;
+//        this -> id = id;
+//        this -> groupn = gn;
+//    };
+//
+//    string getgn(){
+//        return this -> groupn;
+//    }
+//};
 
+int main(){
+//    vector<Student> a;
+    string name, gn;
+    long int id;
+    bool found = false;
+    int n; cin >> n;
+    map<string, int> grn;
+
+    for(int i = 0; i < n; i++){
+        cin >> name >> id >> gn;
+        grn[gn] += 1;
+        //a.emplace_back(name, id, gn);
+    }
+    for(auto i:grn){
+        cout << i.first << ":" << i.second << endl;
+    }
 }
